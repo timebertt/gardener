2ef8878ebd89cf7018b5fb6b5f11fa9b7e33dfd7
Tim Ebert <timebertt@gmail.com>
2022-08-26 16:02:44 +0200

    [WIP] dump events
    TODOs
    - disable framework dump
    - add traps to ci-e2e-*
    - test

--- hack/ci-common.sh
+++ hack/ci-common.sh
@@ -29,19 +29,46 @@ dump_logs() {
       mkdir -p "$node_dir"
 
       # general stuff
-      kubectl -n "$namespace" exec "$node" -- crictl images > "$node_dir/images.log" || true
-      kubectl -n "$namespace" get pod "$node" --show-managed-fields -oyaml > "$node_dir/pod.yaml" || true
+      kubectl -n "$namespace" exec "$node" -- crictl images >"$node_dir/images.log" || true
+      kubectl -n "$namespace" get pod "$node" --show-managed-fields -oyaml >"$node_dir/pod.yaml" || true
 
       # systemd units
-      for unit in cloud-config-downloader kubelet containerd ; do
-        kubectl -n "$namespace" exec "$node" -- journalctl --no-pager -u $unit.service > "$node_dir/$unit.log" || true
+      for unit in cloud-config-downloader kubelet containerd; do
+        kubectl -n "$namespace" exec "$node" -- journalctl --no-pager -u $unit.service >"$node_dir/$unit.log" || true
       done
-      kubectl -n "$namespace" exec "$node" -- journalctl --no-pager > "$node_dir/journal.log" || true
+      kubectl -n "$namespace" exec "$node" -- journalctl --no-pager >"$node_dir/journal.log" || true
 
       # container logs
       kubectl cp "$namespace/$node":/var/log "$node_dir" || true
     done < <(kubectl -n "$namespace" get po -l app=machine -oname | cut -d/ -f2)
   done < <(kubectl get ns -l gardener.cloud/role=shoot -oname | cut -d/ -f2)
+}
+
+dump_events() {
+  dump_events_for_cluster gardener-local-control-plane
+
+  while IFS= read -r shoot; do
+    shoot_namespace="$(echo "$shoot" | awk '{print $1}')"
+    shoot_name="$(echo "$shoot" | awk '{print $2}')"
+    technicalID="$(echo "$shoot" | awk '{print $3}')"
+
+    kubeconfig_file="$(mktemp)"
+    kubectl create -f <(echo '{"apiVersion": "authentication.gardener.cloud/v1alpha1","kind": "AdminKubeconfigRequest","spec": {"expirationSeconds": 1000}}') \
+     --raw "/apis/core.gardener.cloud/v1beta1/namespaces/$shoot_namespace/shoots/$shoot_name/adminkubeconfig" | yq e ".status.kubeconfig" - | base64 -d > "$kubeconfig_file"
+
+    KUBECONFIG="$kubeconfig_file" dump_events_for_cluster "$technicalID"
+    rm -f "$kubeconfig_file"
+  done < <(kubectl get shoot -A -o=custom-columns=namespace:metadata.namespace,name:metadata.name,id:status.technicalID --no-headers)
+}
+
+dump_events_for_cluster() {
+  local eventsdir="$ARTIFACTS/$1/events"
+
+  echo "> Dumping events to $eventsdir"
+  mkdir -p "$eventsdir"
+  while IFS= read -r namespace; do
+    kubectl -n "$namespace" get event --sort-by=lastTimestamp 2>/dev/null > "$eventsdir/$namespace.log" || true
+  done < <(kubectl get ns -oname | cut -d/ -f2)
 }
 
 clamp_mss_to_pmtu() {
