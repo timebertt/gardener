d8822b54b1c79b3f9dde90406be9ef691f9960e8
Tim Ebert <timebertt@gmail.com>
2025-11-24 22:58:38 +0100

    [WIP] gind

---
+++ pkg/provider-local/node/install-gardenadm.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+echo "> Prepare temporary directory for image pull and mount"
+tmp_dir="$(mktemp -d)"
+unmount() {
+  ctr images unmount "$tmp_dir" && rm -rf "$tmp_dir"
+}
+trap unmount EXIT
+
+image="$1"
+
+echo "> Pull gardenadm image and mount it to the temporary directory"
+ctr images pull --hosts-dir "/etc/containerd/certs.d" "$image"
+ctr images mount "$image" "$tmp_dir"
+
+echo "> Copy gardenadm binary to host (/gardenadm) and make it executable"
+mkdir -p "/gardenadm"
+# TODO: use /opt/bin instead
+cp -f "$tmp_dir/ko-app/gardenadm" "/gardenadm"
+chmod +x "/gardenadm/gardenadm"
