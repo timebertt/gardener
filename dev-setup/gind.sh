9e672b99638e6954417f8170dcd65320efe7feba
Tim Ebert <timebertt@gmail.com>
2025-11-25 11:00:38 +0100

    Use `--skip-etcd-druid`

--- dev-setup/gind.sh
+++ dev-setup/gind.sh
@@ -13,11 +13,11 @@ COMPOSE_FILE=$(dirname "$0")/gind/docker-compose.yaml
 
 case "$COMMAND" in
   up)
-    docker compose -f "$COMPOSE_FILE" up -d
+    docker compose -f "$COMPOSE_FILE" up -d --build
 
     make gardenadm-up SCENARIO=gind SKAFFOLD_PLATFORM="linux/$(go env GOARCH)" SKAFFOLD_CHECK_CLUSTER_NODE_PLATFORMS=false
 
-    docker compose -f "$COMPOSE_FILE" exec control-plane bash -c '/install-gardenadm.sh $(cat /gardenadm/.skaffold-image) && gardenadm init -d /gardenadm/gind'
+    docker compose -f "$COMPOSE_FILE" exec control-plane bash -c '/install-gardenadm.sh $(cat /gardenadm/.skaffold-image) && gardenadm init -d /gardenadm/gind --skip-etcd-druid'
 
     docker compose -f "$COMPOSE_FILE" cp control-plane:/etc/kubernetes/admin.conf dev/kubeconfig-gind
     sed -i 's/api.root.garden.local.gardener.cloud/localhost:6443/' dev/kubeconfig-gind
@@ -25,7 +25,7 @@ case "$COMMAND" in
     export KUBECONFIG=dev/kubeconfig-gind
     kubectl taint node --all node-role.kubernetes.io/control-plane- || true
 
-    make operator-up garden-up KUBECONFIG=$KUBECONFIG
+#    make operator-up garden-up KUBECONFIG=$KUBECONFIG
     ;;
 
   down)
